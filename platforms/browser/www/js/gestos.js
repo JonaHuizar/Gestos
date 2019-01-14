32ab3301a4cc263f534a1ebf7e33039252b1f7dd
Jonathan Huizar González <jona_trouble@hotmail.com>
2019-01-13 23:06:46 -0600

    Gestos
    Aplicacion de Gestos

---
+++ platforms/browser/www/js/gestos.js
@@ -0,0 +1,67 @@
+var app={
+	inicio: function(){
+		this.iniciarBotones();
+		this.iniciarFast();
+		this.iniciarHammer();
+	} ,
+
+	iniciarFast: function(){
+		FastClick.attach(document.body);
+	} ,
+
+	iniciarBotones: function(){
+		var botonClaro= document.querySelector('#claro');
+		var botonOscuro= document.querySelector('#oscuro');
+
+		botonClaro.addEventListener('click', this.ponloClaro,false);
+		botonOscuro.addEventListener('click', this.ponloOscuro,false);
+	} ,
+
+	iniciarHammer: function(){
+		var zona= document.getElementById('zona-gestos');
+		var hammertime = new Hammer(zona);
+
+		hammertime.get('pinch').set({enable: true});
+		hammertime.get('rotate').set({enable: true});
+		
+		zona.addEventListener('webkitAnimationEnd', function(e){
+			zona.className='';
+		});
+
+		hammertime.on('doubletap', function(ev){
+			zona.className='doubletap';
+		});
+
+		hammertime.on('press', function(ev){
+			zona.className='press';
+		});
+
+		hammertime.on('swipe', function(ev){
+			var clase=undefined;
+			direccion=ev.direction;
+			if(direccion==4) clase='swipe-derecha';
+			if(direccion==2) clase='swipe-izquierda';
+
+			zona.className=clase;
+		});
+
+		hammertime.on('rotate', function(ev){
+			var umbral=25;
+			if(ev.distance > umbral) zona.className='rotate';
+		});
+	} ,
+
+	ponloClaro: function(){
+		document.body.className='claro';
+	} ,
+
+	ponloOscuro: function(){
+		document.body.className='oscuro';
+	} ,
+};
+
+if('addEventListener' in document){
+	document.addEventListener('DOMContentLoaded', function(){
+		app.inicio();
+	}, false);
+}
